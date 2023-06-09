2d8c9f7fe1003d8758b05b03da575719e17a935b
Samy Samer <106012138+samySamer@users.noreply.github.com>
2023-06-09 04:07:00 +0200

    Finished Till Event Binding

--- Angular intro/my-first-app/src/app/servers/servers.component.ts
+++ Angular intro/my-first-app/src/app/servers/servers.component.ts
@@ -5,4 +5,15 @@ import { Component } from '@angular/core';
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
-export class ServersComponent {}
+export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No servers Created!';
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was Created! ';
+  }
+}
